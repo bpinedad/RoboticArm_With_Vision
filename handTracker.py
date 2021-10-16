9de979ec00e3676fefce8830ec4aa43ca85e53fc
bpinedad <bpinedad96@gmail.com>
2021-10-15 20:44:34 -0500

    Implemented ROS

--- handTracker.py
+++ handTracker.py
@@ -1,12 +1,18 @@
+#!/usr/bin/env python
+
 import cv2
 import mediapipe as mp
 import time
 import math
 
+#ROS dependencies
+import rospy
+from std_msgs.msg import Int32MultiArray    
+    
 class handDetector():
 
     #Initialization with default parameters
-    def __init__(self, mode = False, maxHands = 2, detectionCon = 0.5, trackCon = 0.5):
+    def __init__(self, mode = False, maxHands = 1, detectionCon = 0.5, trackCon = 0.5):
         self.mode = mode
         self.maxHands = maxHands
         self.detectionCon = detectionCon
@@ -47,10 +53,15 @@ def getDistance (x1, x2, y1, y2):
 def main():
     pTime = 0
     cTime = 0
-    cap = cv2.VideoCapture(2)
+    cap = cv2.VideoCapture(0)
     detector = handDetector()
 
-    while True:
+    pub = rospy.Publisher('hand_status', Int32MultiArray, queue_size=10)
+    rospy.init_node('hands', anonymous=True)
+    rate = rospy.Rate(10) # 10hz
+
+    while not rospy.is_shutdown():
+
         ret, img = cap.read()
         H, W, channels = img.shape
         mirror = cv2.flip(img, 1)
@@ -77,6 +88,24 @@ def main():
             #print(f'Distance from Open: {distanceMiddle}')
             #print(handPoints[0])
             #print(handPoints[5])
+
+            #Goes from 0 to W, servo should be from 0 to 180
+            s1 = math.floor((handPoints[9][1])*180/(W))
+            #Goes from 0 to H, servo should be from 0 to 180
+            s2 = math.floor((handPoints[9][2])*180/(H))
+            #Goes from 25 to 50, servo should be from 35 to 180
+            s3 = math.floor(distanceCam * (180 - 25) / 50)+25
+            #Goes from 0 to 1, just open or close
+            s4 = 3 if hand == 'Open' else 108
+
+            #Round to multiples of 5
+            s1 += s1%3
+            s2 += s2%3
+            s3 += s3%3
+
+            msg = Int32MultiArray()
+            msg.data = [s1,s2,s3,s4]
+            pub.publish(msg)
 
         cv2.imshow("Image", img)
         if cv2.waitKey(1) == 27:
